d3ec8a2920fba999b7c594758aadc0f54bf41631
Dolidodzik <teethtard321@gmail.com>
2022-07-03 21:53:14 +0200

    added linter

--- src/App.js
+++ src/App.js
@@ -1,49 +1,39 @@
-import React, { useState } from "react";
-import "./App.css";
+import React, { useState } from 'react'
+import './App.css'
 
 function App() {
-  
-  const [newItem, setNewItem] = useState("");
+
+  const [newItem, setNewItem] = useState('');
   const [items, setItems] = useState([]);
 
   const [showEdit, setShowEdit] = useState();
-  const [updatedText, setUpdatedText] = useState("");
+  const [updatedText, setUpdatedText] = useState('');
 
- 
-
-  
   function addItem() {
-    
-    if (!newItem) {
-      alert("Press enter an item.");
+
+    if (!newItem) {alert('Press enter an item.');
       return;
     }
-
     const item = {
       id: Math.floor(Math.random() * 2137),
       value: newItem,
     };
 
-    
     setItems((prevList) => [...prevList, item]);
-
-   
-    setNewItem("");
+    setNewItem('');
   }
-  
-  
+
+
   function deleteItem(id) {
     const newArray = items.filter((item) => item.id !== id);
-    setItems(newArray);
-  }
+    setItems(newArray); }
 
-  
   function editItem(id, newText) {
-    
+
     const currentItem = items.filter((item) => item.id === id);
-    
+
     if(!updatedText){
-      alert("Press enter an item.");
+      alert('Press enter an item.');
       return;
     }
 
@@ -51,15 +41,11 @@ function App() {
       id: currentItem.id,
       value: newText,
     };
-    
-    
 
-    
     deleteItem(id);
 
-    
     setItems((prevList) => [...prevList, updatedItem]);
-    setUpdatedText("");
+    setUpdatedText('');
     setShowEdit(0);
   }
 
@@ -73,28 +59,23 @@ function App() {
     }else{
       return(<p>{items.length} Task left</p>)
     }
+  }
 
-  } 
-  
   return (
-    <div className="app">
-      
+    <div className='app'>
+
       <h1>Todo List</h1>
       {taskLeft()}
 
-      
-      <input className="input"
-        type="text"
-        placeholder="Add an item"
+      <input className='input'
+        type='text'
+        placeholder='Add an item'
         value={newItem}
         onChange={(e) => setNewItem(e.target.value)}
       />
-      
 
-      
       <button className='button' onClick={() => addItem()}>Add</button>
 
-      
       <ul>
         {items.map((item) => {
           return (
@@ -102,7 +83,7 @@ function App() {
               <li className='div' key={item.id} >
                 {item.value}
                 <button
-                  className="delete-button"
+                  className='delete-button'
                   onClick={() => deleteItem(item.id)}
                 >
                   ❌
@@ -112,16 +93,16 @@ function App() {
 
               {showEdit == item.id ? (
                 <div>
-                  <input className="input"
-                    type="text"
-                    placeholder="Update item"
+                  <input className='input'
+                    type='text'
+                    placeholder='Update item'
                     value = {updatedText}
                     onChange={(e) => setUpdatedText(e.target.value)}
                   />
-                  <button className="button" onClick={() => editItem(item.id, updatedText)}>
+                  <button className='button' onClick={() => editItem(item.id, updatedText)}>
                     Update
                   </button>
-                  <button onClick={sth} className="delete-button">❌</button>
+                  <button onClick={sth} className='delete-button'>❌</button>
                 </div>
               ) : null }
             </div>
